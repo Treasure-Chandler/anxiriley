1fa7c0dd9aca31d9be775df3ebe562c1ae53ed23
Treasure-Chandler <tchandler1211@gmail.com>
2025-03-11 20:47:33 -0400

    Finished popup functionality, started login page and added the background for it

---
+++ Main Pages/login.js
@@ -0,0 +1,6 @@
+/**
+ * @author Treasure Chandler
+ * 
+ * This allows full functionality of the login page, which includes gmail authentation and Firebase integration
+ * for account handling.
+ */
---
+++ Main Popup/hello.js
@@ -0,0 +1,10 @@
+/**
+ * @author Treasure Chandler
+ * 
+ * This allows the full extension to be opened in a new tab when the Wildcat is clicked.
+ */
+
+// Open event
+document.getElementById('openLogin').addEventListener('click', function() {
+    chrome.tabs.create({ url: '/Main Pages/login.html' });
+});
