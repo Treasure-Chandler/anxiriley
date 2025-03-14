5faf8616bb7cf209fc3d1e2d9bb60e942086ac32
Treasure-Chandler <tchandler1211@gmail.com>
2025-03-14 18:22:01 -0400

    Finished the design for the login page, along with making slight edits to the popup

--- Main Pages/login.js
+++ Main Pages/login.js
@@ -1,6 +1,6 @@
 /**
  * @author Treasure Chandler
  * 
- * This allows full functionality of the login page, which includes gmail authentation and Firebase integration
+ * This allows full functionality of the login page, which includes Gmail authentation and Firebase integration
  * for account handling.
  */
