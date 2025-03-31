9b768a93190d6c675f403fbb95924cbc245c6194
Treasure-Chandler <tchandler1211@gmail.com>
2025-03-31 13:30:42 -0400

    More progress on the sign up popup

--- Main Pages/login.js
+++ Main Pages/login.js
@@ -4,3 +4,13 @@
  * This allows full functionality of the login page, which includes Gmail authentation and Firebase integration
  * for account handling.
  */
+
+// Open the sign up form when the "Sign Up" button is clicked
+function openSignUp() {
+    document.getElementById("signUp").style.display = "block";
+}
+
+// Close the sign up form when the "X" button is clicked
+function closeSignUp() {
+    document.getElementById("signUp").style.display = "none";
+}
